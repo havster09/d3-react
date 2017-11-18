30f662ebdcb423f6ce505e5b9c5d64970882c776
haven <haven@actio.com.au>
2017-11-18 13:44:19 +1100

    hocFactory

--- src/containers/hocii/index.js
+++ src/containers/hocii/index.js
@@ -36,6 +36,7 @@ class SmInput extends React.Component {
 }
 
 class Stringifier extends React.Component {
+  static displayName = `Stringifier`;
   constructor(props) {
     super(props);
     this.smMethod = this.smMethod.bind(this);
@@ -47,7 +48,7 @@ class Stringifier extends React.Component {
   smMethod() {
     console.log(this.props);
     setTimeout(() => {
-      this.setState({ say: 'u2' });
+      this.setState({ say: 'u2', value: 'whoever' });
     }, 1000);
   }
 
@@ -57,6 +58,7 @@ class Stringifier extends React.Component {
         <pre>
           {JSON.stringify(this.props)}
         </pre>
+        <input type="text" value={this.state.value} />
         <p>
           {this.state.say}
         </p>
@@ -67,12 +69,22 @@ class Stringifier extends React.Component {
 
 const iiHoc = WrappedComponent => {
   return class Enhancer extends WrappedComponent {
+    static displayName = `IIHOC(${WrappedComponent.displayName ||
+      'Component'})`;
     constructor(props) {
       super(props);
       this.state = {
         value: 'vr mk'
       };
+      this.getDisplayName = this.getDisplayName.bind(this);
     }
+
+    getDisplayName(WrappedComponent) {
+      return (
+        WrappedComponent.displayName || WrappedComponent.name || 'Component'
+      );
+    }
+
     handleChange = event => {
       this.setState({ value: event.target.value });
     };
@@ -117,13 +129,73 @@ const iiHoc = WrappedComponent => {
 const HocStringifier = iiHoc(Stringifier);
 const HocInput = iiHoc(SmInput);
 
+const HocFac = params => WrappedComponent => {
+  return class HocWithParams extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleInvokeParamMethod = this.handleInvokeParamMethod.bind(this);
+    }
+    handleInvokeParamMethod() {
+      params.method();
+    }
+    componentDidMount() {
+      params.method();
+    }
+    render() {
+      return (
+        <div>
+          <WrappedComponent value={params.value} />
+          <button onClick={this.handleInvokeParamMethod}>Invoke</button>
+        </div>
+      );
+    }
+  };
+};
+
+const HocFacExample = HocFac({value: 'ey', method: () => {console.log(`gonna dog u`)}})(SmInput); //instead of string can add method
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleForceUpdate = this.handleForceUpdate.bind(this);
+  }
+  handleForceUpdate() {
+    this.forceUpdate();
+  }
+  render() {
+    const mutatedChildren = this.props.children.map((child,i) => {
+      const mutantChild = React.cloneElement(
+        child,
+        { key: i,style: { fontSize: Math.ceil(Math.random() * 20 + 10) } },
+        Math.random() * 20
+      );
+      return mutantChild;
+    });
+    return (
+      <div>
+        {mutatedChildren}
+        <button onClick={this.handleForceUpdate}>Render</button>
+      </div>
+    );
+  }
+}
+
 const HocIi = props =>
   <div>
     <Paper zDepth={1} style={styles.paper}>
+      <HocFacExample />
+      <hr />
       <h1>Hoc</h1>
       <HocStringifier smNum={50} />
       <HocInput smNum={75} />
       <HocStringifier smNum={100} />
+      <Parent>
+        <p>fun in the s</p>
+        <p>fun in the s</p>
+        <p>fun in the s</p>
+        <p>fun in the s</p>
+        <p>fun in the s</p>
+      </Parent>
     </Paper>
   </div>;
 
